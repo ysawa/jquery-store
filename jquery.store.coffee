c51eaffd8dba2ade24b23f8259c6a6b026d84814
Y Sawa <yoshihirosawa@gmail.com>
2012-10-12 11:51:09 +0900

    refactor: around JSON

--- jquery.store.coffee
+++ jquery.store.coffee
@@ -3,21 +3,6 @@
 # jQuery Store can store almost all type of objects.
 
 (($) ->
-  special_chars =
-    "\b": "\\b"
-    "\t": "\\t"
-    "\n": "\\n"
-    "\f": "\\f"
-    "\r": "\\r"
-    "\"": "\\\""
-    "\\": "\\\\"
-  escape_char = (char) ->
-    special_chars[char] or "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4)
-  if window.JSON and window.JSON.stringify
-    JSON = window.JSON
-  else
-    JSON = null
-
   $.extend
     store:
       generate_key: (key) ->
@@ -33,11 +18,11 @@
         if typeof value_string == 'undefined' or value_string == null
           value_string
         else
-          $.parseJSON(value_string)
+          @parse_json(value_string)
       initialize: ->
         @storage_valid = false
         unless typeof localStorage == 'undefined'
-          test_key = 'jqstore__test'
+          test_key = @generate_key('_test')
           localStorage.setItem(test_key, 'valid')
           value = localStorage.getItem(test_key)
           if value and value == 'valid'
@@ -48,6 +33,23 @@
           @storage = localStorage
         else
           @storage = {}
+        if window.JSON and window.JSON.stringify
+          @json_object = window.JSON
+        else
+          @json_object = null
+      json_object: null
+      json_special_characters:
+        "\b": "\\b"
+        "\t": "\\t"
+        "\n": "\\n"
+        "\f": "\\f"
+        "\r": "\\r"
+        "\"": "\\\""
+        "\\": "\\\\"
+      json_escape_character: (character) ->
+        @json_special_characters[character] or
+          "\\u" + ("0000" + character.charCodeAt(0).toString(16)).slice(-4)
+      parse_json: $.parseJSON
       prefix: 'jqstore_'
       remove: (key) ->
         key = @generate_key(key)
@@ -61,7 +63,7 @@
         else
           @storage = {}
       set: (key, value) ->
-        value_string = $.stringifyJSON(value)
+        value_string = @stringify_json(value)
         key = @generate_key(key)
         if @storage_valid
           @storage.setItem(key, value_string)
@@ -69,38 +71,45 @@
           @storage[key] = value_string
       storage: {}
       storage_valid: false
-    stringifyJSON: (data) ->
-      return JSON.stringify(data) if JSON
-      switch $.type(data)
-        when "string"
-          return "\"" + data.replace(/[\x00-\x1f\\"]/g, escape_char) + "\""
-        when "array"
-          return "[" + $.map(data, $.stringifyJSON) + "]"
-        when "object"
-          string = []
-          $.each data, (key, value) ->
-            json = $.stringifyJSON(value)
-            string.push $.stringifyJSON(key) + ":" + json if json
-          return "{" + string + "}"
-        when "number", "boolean"
-          return "" + data
-        when "date"
-          year = data.getUTCFullYear()
-          month = data.getUTCMonth() + 1
-          day = data.getUTCDate()
-          hour = data.getUTCHours()
-          minute = data.getUTCMinutes()
-          second = data.getUTCSeconds()
-          milli = data.getUTCMilliseconds()
-          month = "0" + month if month < 10
-          day = "0" + day if day < 10
-          hour = "0" + hour if hour < 10
-          minute = "0" + minute if minute < 10
-          second = "0" + second if second < 10
-          return "\"#{year}-#{month}-#{day}T#{hour}:#{minute}:#{second}.#{milli}Z\""
-        when "undefined", "null"
-          return "null"
-      data
+      stringify_json: (data) ->
+        return @json_object.stringify(data) if @json_object
+        that = @
+        switch $.type(data)
+          when "string"
+            json_escape_character = (string) ->
+              that.json_escape_character(string)
+            return "\"" + data.replace(/[\x00-\x1f\\"]/g, json_escape_character) + "\""
+          when "array"
+            stringify_json = (object) ->
+              that.stringify_json(object)
+            return "[" + $.map(data, stringify_json) + "]"
+          when "object"
+            string = []
+            stringify_json = (object) ->
+              that.stringify_json(object)
+            $.each data, (key, value) ->
+              json = stringify_json(value)
+              string.push stringify_json(key) + ":" + json if json
+            return "{" + string + "}"
+          when "number", "boolean"
+            return "" + data
+          when "date"
+            year = data.getUTCFullYear()
+            month = data.getUTCMonth() + 1
+            day = data.getUTCDate()
+            hour = data.getUTCHours()
+            minute = data.getUTCMinutes()
+            second = data.getUTCSeconds()
+            milli = data.getUTCMilliseconds()
+            month = "0" + month if month < 10
+            day = "0" + day if day < 10
+            hour = "0" + hour if hour < 10
+            minute = "0" + minute if minute < 10
+            second = "0" + second if second < 10
+            return "\"#{year}-#{month}-#{day}T#{hour}:#{minute}:#{second}.#{milli}Z\""
+          when "undefined", "null"
+            return "null"
+        data
 
   $.store.initialize()
 ) jQuery
--- jquery.store.js
+++ jquery.store.js
@@ -1,24 +1,6 @@
 (function() {
 
   (function($) {
-    var JSON, escape_char, special_chars;
-    special_chars = {
-      "\b": "\\b",
-      "\t": "\\t",
-      "\n": "\\n",
-      "\f": "\\f",
-      "\r": "\\r",
-      "\"": "\\\"",
-      "\\": "\\\\"
-    };
-    escape_char = function(char) {
-      return special_chars[char] || "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4);
-    };
-    if (window.JSON && window.JSON.stringify) {
-      JSON = window.JSON;
-    } else {
-      JSON = null;
-    }
     $.extend({
       store: {
         generate_key: function(key) {
@@ -37,14 +19,14 @@
           if (typeof value_string === 'undefined' || value_string === null) {
             return value_string;
           } else {
-            return $.parseJSON(value_string);
+            return this.parse_json(value_string);
           }
         },
         initialize: function() {
           var test_key, value;
           this.storage_valid = false;
           if (typeof localStorage !== 'undefined') {
-            test_key = 'jqstore__test';
+            test_key = this.generate_key('_test');
             localStorage.setItem(test_key, 'valid');
             value = localStorage.getItem(test_key);
             if (value && value === 'valid') {
@@ -52,11 +34,30 @@
             }
           }
           if (this.storage_valid) {
-            return this.storage = localStorage;
+            this.storage = localStorage;
           } else {
-            return this.storage = {};
+            this.storage = {};
+          }
+          if (window.JSON && window.JSON.stringify) {
+            return this.json_object = window.JSON;
+          } else {
+            return this.json_object = null;
           }
         },
+        json_object: null,
+        json_special_characters: {
+          "\b": "\\b",
+          "\t": "\\t",
+          "\n": "\\n",
+          "\f": "\\f",
+          "\r": "\\r",
+          "\"": "\\\"",
+          "\\": "\\\\"
+        },
+        json_escape_character: function(character) {
+          return this.json_special_characters[character] || "\\u" + ("0000" + character.charCodeAt(0).toString(16)).slice(-4);
+        },
+        parse_json: $.parseJSON,
         prefix: 'jqstore_',
         remove: function(key) {
           key = this.generate_key(key);
@@ -75,7 +76,7 @@
         },
         set: function(key, value) {
           var value_string;
-          value_string = $.stringifyJSON(value);
+          value_string = this.stringify_json(value);
           key = this.generate_key(key);
           if (this.storage_valid) {
             return this.storage.setItem(key, value_string);
@@ -84,60 +85,70 @@
           }
         },
         storage: {},
-        storage_valid: false
-      },
-      stringifyJSON: function(data) {
-        var day, hour, milli, minute, month, second, string, year;
-        if (JSON) {
-          return JSON.stringify(data);
-        }
-        switch ($.type(data)) {
-          case "string":
-            return "\"" + data.replace(/[\x00-\x1f\\"]/g, escape_char) + "\"";
-          case "array":
-            return "[" + $.map(data, $.stringifyJSON) + "]";
-          case "object":
-            string = [];
-            $.each(data, function(key, value) {
-              var json;
-              json = $.stringifyJSON(value);
-              if (json) {
-                return string.push($.stringifyJSON(key) + ":" + json);
+        storage_valid: false,
+        stringify_json: function(data) {
+          var day, hour, json_escape_character, milli, minute, month, second, string, stringify_json, that, year;
+          if (this.json_object) {
+            return this.json_object.stringify(data);
+          }
+          that = this;
+          switch ($.type(data)) {
+            case "string":
+              json_escape_character = function(string) {
+                return that.json_escape_character(string);
+              };
+              return "\"" + data.replace(/[\x00-\x1f\\"]/g, json_escape_character) + "\"";
+            case "array":
+              stringify_json = function(object) {
+                return that.stringify_json(object);
+              };
+              return "[" + $.map(data, stringify_json) + "]";
+            case "object":
+              string = [];
+              stringify_json = function(object) {
+                return that.stringify_json(object);
+              };
+              $.each(data, function(key, value) {
+                var json;
+                json = stringify_json(value);
+                if (json) {
+                  return string.push(stringify_json(key) + ":" + json);
+                }
+              });
+              return "{" + string + "}";
+            case "number":
+            case "boolean":
+              return "" + data;
+            case "date":
+              year = data.getUTCFullYear();
+              month = data.getUTCMonth() + 1;
+              day = data.getUTCDate();
+              hour = data.getUTCHours();
+              minute = data.getUTCMinutes();
+              second = data.getUTCSeconds();
+              milli = data.getUTCMilliseconds();
+              if (month < 10) {
+                month = "0" + month;
               }
-            });
-            return "{" + string + "}";
-          case "number":
-          case "boolean":
-            return "" + data;
-          case "date":
-            year = data.getUTCFullYear();
-            month = data.getUTCMonth() + 1;
-            day = data.getUTCDate();
-            hour = data.getUTCHours();
-            minute = data.getUTCMinutes();
-            second = data.getUTCSeconds();
-            milli = data.getUTCMilliseconds();
-            if (month < 10) {
-              month = "0" + month;
-            }
-            if (day < 10) {
-              day = "0" + day;
-            }
-            if (hour < 10) {
-              hour = "0" + hour;
-            }
-            if (minute < 10) {
-              minute = "0" + minute;
-            }
-            if (second < 10) {
-              second = "0" + second;
-            }
-            return "\"" + year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "." + milli + "Z\"";
-          case "undefined":
-          case "null":
-            return "null";
+              if (day < 10) {
+                day = "0" + day;
+              }
+              if (hour < 10) {
+                hour = "0" + hour;
+              }
+              if (minute < 10) {
+                minute = "0" + minute;
+              }
+              if (second < 10) {
+                second = "0" + second;
+              }
+              return "\"" + year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "." + milli + "Z\"";
+            case "undefined":
+            case "null":
+              return "null";
+          }
+          return data;
         }
-        return data;
       }
     });
     return $.store.initialize();
